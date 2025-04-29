6821606d10198940d9383f34fb0fde2246f6d2ee
saddam <saddam.dermawan@gmail.com>
2025-04-29 10:23:34 +0700

    tugas 2

---
+++ tugas2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        define('Nama_Kamu', 'Saddam');
+        define('Alamat', 'Saddam Alamat');
+        define('TTL', 'Jakarta, 1 Januari 2000');
+        define('Telp', '08123456789');
+
+        echo "Nama Kamu: " . Nama_Kamu . "<br>";
+        echo "Alamat: " . Alamat . "<br>";
+        echo "TTL: " . TTL . "<br>";
+        echo "Telp: " . Telp . "<br>";
+    ?>
+</body>
+</html>
